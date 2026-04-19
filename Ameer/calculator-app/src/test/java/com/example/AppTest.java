b749f2777d4ba72685c48a4e2efb18c99e77e310
ameer502311 <ameer.502311@sxcce.edu.in>
2026-04-19 20:08:29 +0530

    Add files via upload

---
+++ Ameer/calculator-app/src/test/java/com/example/AppTest.java
@@ -0,0 +1,10 @@
+package com.example;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class AppTest {
+
+    @Test
+    void testAdd() {
+        assertEquals(5, 2 + 3);
+    }
+}
